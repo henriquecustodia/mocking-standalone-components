16a75eb432c6be0a3233d248a37774a86fecd88b
Henrique Custódia <martinshenrique8@gmail.com>
2024-07-03 08:45:22 -0300

    feat: cria custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CustomComponent } from './shared/components/custom/custom.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CustomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
